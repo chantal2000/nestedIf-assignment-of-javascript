5e23986eea35badcab6ffc5e6e55d8ca1f29c657
chantal2000 <nniyonkuruchantal@gmail.com>
2021-03-16 11:05:26 +0300

    first commit

---
+++ jj.js
@@ -0,0 +1,17 @@
+var student={
+    name:"Chantal",
+    age:30,
+    weight:"599kgs",
+}
+var age="name";
+console.log(student.age);
+console.log(student[age]);
+console.log(student['name']);
+var a=[1,2,3];
+var b=['1,2,3'];
+var c=[1,2,3];
+var f='1,2,3'
+console.log(a==b);
+console.log(a==c);
+console.log(b==c);
+console.log(a==f);
